923b5dcd62a45b63839b344ad46e9446328c1ad4
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2021-07-14 13:29:59 +0300

    Add files via upload

---
+++ Hash/Result.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionHash
+{
+    public class Result
+    {
+        public string hashType { get; set; }
+        public string message { get; set; }
+        public string hashValue { get; set; }
+    }
+}
